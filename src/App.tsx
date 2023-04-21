abd28b349a74ac00d7e9d68dd169d312cdb9ef99
Baraka-Mugisha <mugishaje@gmail.com>
2023-04-21 16:32:08 +0200

    added qeustions page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Quiz from "./pages/quiz";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link text-3xl font-bold underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/quiz" element={<Quiz />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/question.tsx
@@ -0,0 +1,61 @@
+import { Typography, Button } from "@mui/material";
+import Options from "./options";
+import React, { useState } from "react";
+
+interface Option {
+  id: string;
+  value: string;
+  label: string;
+}
+
+interface QuizProps {
+  question: string;
+  options: Array<Option>;
+  correctAnswer: string;
+  goToNext: (isCorrect: boolean) => void;
+  onOptionSelect: (selectedOption: string) => void; // add this line
+}
+
+const QuestionComponent = ({
+  question,
+  options,
+  correctAnswer,
+  goToNext,
+  onOptionSelect,
+}: QuizProps) => {
+  const [selectedOption, setSelectedOption] = useState<string>("");
+  const [isAnswered, setIsAnswered] = useState<boolean>(false);
+
+  const handleAnswer = () => {
+    const isCorrect = selectedOption === correctAnswer;
+    setIsAnswered(true);
+    goToNext(isCorrect);
+  };
+
+  return (
+    <div className="m-auto w-96">
+      <Typography variant="h5">{question}</Typography>
+      <Options
+        className="text-center m-auto"
+        options={options}
+        selectedOption={selectedOption}
+        onChange={(value: string) => {
+          setSelectedOption(value);
+          onOptionSelect(value)
+        }}
+        isAnswered={isAnswered}
+        correctAnswer={correctAnswer}
+      />
+      <Button
+        className="my-button"
+        disabled={!selectedOption}
+        variant="text"
+        onClick={handleAnswer}
+      >
+        {isAnswered ? "Next" : "Submit"}
+      </Button>
+    </div>
+  );
+};
+
+export default QuestionComponent;
